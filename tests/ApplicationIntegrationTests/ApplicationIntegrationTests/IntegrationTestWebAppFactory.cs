0ef20785ea5cad682b784d119bc6a7712f776dc0
Ataulhaq Akbar <ataul.akbar@gmail.com>
2023-12-27 21:44:20 -0500

    removed the old code

--- tests/ApplicationIntegrationTests/ApplicationIntegrationTests/IntegrationTestWebAppFactory.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc.Testing;
-
-namespace ApplicationIntegrationTests;
-
-public class IntegrationTestWebAppFactory : WebApplicationFactory<Project> // I'll need to create a reference by adding a porject likek the postgresql database project
-{
-    
-}
